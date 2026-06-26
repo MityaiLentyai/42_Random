d86791f5202428ee144dd5903b510f6734f42a54
MityaiLentyai <luckmanpackman@gmail.com>
2026-06-26 21:53:26 +0200

    strtrim done

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dzzayats <dzzayats@student.42warsaw.pl>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/25 20:33:16 by dzzayats          #+#    #+#             */
-/*   Updated: 2026/06/26 02:40:36 by dzzayats         ###   ########.fr       */
+/*                                                       :::      ::::::::    */
+/*   ft_strtrim.c                                      :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: dzzayats <dzzayats@student.42warsaw.pl>   #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/06/25 20:33:16 by dzzayats         #+#    #+#              */
+/*   Updated: 2026/06/26 21:52:46 by dzzayats        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,36 +23,88 @@ from the beginning and the end.
 */
 
 #include "libft.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-char *ft_strtrim(char const *s1, char const *set)
+int	is_set(const char c, const char *set)
 {
-	char	*substr;
-	size_t	set_iterator;
-	size_t	string_iterator;
+	int	i;
+	int	is_set;
 
-	if ((!s1) || (!set))
-		return (NULL);
-	
-	string_iterator = 0;
-	while (s1[string_iterator] && ft_strchr(s1,(char)set[set_iterator]))
+	is_set = 0;
+	i = 0;
+	while (set[i])
 	{
-		string_iterator++;
+		if (set[i] == c)
+			is_set = 1;
+		i++;
 	}
-	*(s1+string_iterator);
-	string_iterator = ft_strlen(s1);
-	while (string_iterator > 0 && ft_strchr(s1,(char)set[set_iterator]))
-	{
-		string_iterator++;
-	}
-	return (substr);
+	return (is_set);
 }
 
-// #include <stdio.h>
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	i;
+	char	*ret;
 
-// int main()
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && is_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && is_set(s1[end - 1], set))
+		end--;
+	ret = malloc(end - start + 1);
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (i < (end - start))
+	{
+		ret[i] = s1[start + i];
+		i++;
+	}
+	ret[end - start] = '\0';
+	return (ret);
+}
+
+// static void print_trim_test(char const * s1,
+// 	char const	*set, char const *expected)
 // {
-// 	char *s1 = Alma materal;
+// 	char		*result;
+// 	const char	*s1_text = s1 ? s1 : "(null)";
+// 	const char	*set_text = set ? set : "(null)";
+// 	const char	*expected_text = expected ? expected : "(null)";
 
+// 	result = ft_strtrim(s1, set);
+// 	if (result)
+// 	{
+// 		printf("s1=%s, set=%s => result=%s, expected=%s\n",
+// 			s1_text, set_text, result, expected_text);
+// 		free(result);
+// 	}
+// 	else
+// 	{
+// 		printf("s1=%s, set=%s => result=(null), expected=%s\n",
+// 			s1_text, set_text, expected_text);
+// 	}
+// }
 
+// int	main(void)
+// {
+// 	print_trim_test("abcba", "ca", "bcb");
+// 	print_trim_test("   hello   ", " ", "hello");
+// 	print_trim_test("xxxabcxxx", "x", "abc");
+// 	print_trim_test("abc", "", "abc");
+// 	print_trim_test("", "abc", "");
+// 	print_trim_test("aaaa", "a", "");
+// 	print_trim_test("abc", "xyz", "abc");
+// 	print_trim_test("   abc   ", " abc", "");
+// 	print_trim_test("abacaba", "ab", "c");
+// 	print_trim_test("  \t  test\n  ", " \t\n", "test");
+// 	print_trim_test(NULL, "abc", NULL);
+// 	print_trim_test("abc", NULL, NULL);
+// 	return (0);
 // }
